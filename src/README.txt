f2a1fe991778474663be80ad7de02a4b29891788
Barb Cutler <Barb Cutler>
2023-02-16 02:02:01 -0500

    hw3 provided code

---
+++ src/README.txt
@@ -0,0 +1,124 @@
+HOMEWORK 3: RAY TRACING, RADIOSITY, & PHOTON MAPPING
+
+NAME:  < insert name >
+
+
+
+ESTIMATE OF # OF HOURS SPENT ON THIS ASSIGNMENT:  < insert # hours >
+
+
+
+COLLABORATORS AND OTHER RESOURCES: List the names of everyone you
+talked to about this assignment and all of the resources (books,
+online reference material, etc.) you consulted in completing this
+assignment.
+
+< insert collaborators / resources >
+
+Remember: Your implementation for this assignment must be done on your
+own, as described in "Academic Integrity for Homework" handout.
+
+
+
+OPERATING SYSTEM & VERSION & GRAPHICS CARD:  < insert info... >
+
+
+
+SELF GRADING TOTAL:  [ < ?? > / 20 ]
+
+
+< Please insert notes on the implementation, known bugs, extra credit
+in each section. >
+
+
+
+2 PROGRESS POSTS [ ?? / 5 ] Posted on Submitty Discussion Forum on the
+dates specified on the calendar.  Includes short description of
+status, and at least one image.  Reasonable progress has been made.
+
+
+SPHERE INTERSECTIONS, SHADOWS, & REFLECTION [ ?? / 2 ]
+  ./render -size 200 200 -input reflective_spheres.obj 
+  ./render -size 200 200 -input reflective_spheres.obj -num_bounces 1
+  ./render -size 200 200 -input reflective_spheres.obj -num_bounces 3 -num_shadow_samples 1 
+
+
+RAY TREE VISUALIZATION OF SHADOW & REFLECTIVE RAYS [ ?? / 1 ]
+
+
+DISTRIBUTION RAY TRACING: SOFT SHADOWS & ANTIALIASING [ ?? / 2 ]
+  ./render -size 200 200 -input textured_plane_reflective_sphere.obj -num_bounces 1 -num_shadow_samples 1
+  ./render -size 200 200 -input textured_plane_reflective_sphere.obj -num_bounces 1 -num_shadow_samples 4
+  ./render -size 200 200 -input textured_plane_reflective_sphere.obj -num_bounces 1 -num_shadow_samples 9 -num_antialias_samples 9
+
+
+EXTRA CREDIT: SAMPLING [ ?? ]
+1 point for stratified sampling of pixel in image plane
+1 point for stratified sampling of soft shadows
+includes discussion of performance/quality
+
+
+OTHER DISTRIBUTION RAY TRACING EXTRA CREDIT [ ?? ]
+glossy surfaces, motion blur, or depth of field, etc.
+
+
+BASIC FORM FACTOR COMPUTATION [ ?? / 2 ]
+Description of method in README.txt.  
+  ./render -size 200 200 -input cornell_box.obj
+
+
+RADIOSITY SOLVER [ ?? / 3 ]
+May be iterative (solution fades in) or done by inverting the form
+factor matrix.
+
+
+FORM FACTORS WITH VISIBILITY / OCCLUSION RAY CASTING [ ?? / 1 ]
+  ./render -size 300 150 -input l.obj 
+  ./render -size 300 150 -input l.obj -num_form_factor_samples 100
+  ./render -size 300 150 -input l.obj -num_shadow_samples 1 
+  ./render -size 300 150 -input l.obj -num_form_factor_samples 10 -num_shadow_samples 1 
+  ./render -size 200 200 -input cornell_box_diffuse_sphere.obj -sphere_rasterization 16 12
+  ./render -size 200 200 -input cornell_box_diffuse_sphere.obj -sphere_rasterization 16 12 -num_shadow_samples 1
+
+
+RADIOSITY EXTRA CREDIT [ ?? ]
+1 point for ambient term in radiosity
+1-2 points for new test scene or visualization
+1 point for writing the ray traced image to a file
+1-3 points extra credit for performance improvements
+1-3 points for other ray tracing effects
+1-3 points for gradient or discontinuity meshing in radiosity 
+
+
+PHOTON DISTRIBUTION [ ?? / 2 ]
+Shoots photons into the scene and the visualization looks reasonable
+(the heart shape can be seen in the ring).
+  ./render -size 200 200 -input reflective_ring.obj -num_photons_to_shoot 10000 -num_bounces 2 -num_shadow_samples 10
+  ./render -size 200 200 -input reflective_ring.obj -num_photons_to_shoot 500000 -num_bounces 2 -num_shadow_samples 10 -num_antialias_samples 4
+
+
+RAY TRACING WITH PHOTONS [ ?? / 2 ]
+Searching for photons in the kdtree to use in ray tracing.  The
+caustic ring is visible, and there are no significant artifacts in
+illumination.
+
+
+PHOTON MAPPING MATCHES RADIOSITY [ ?? ]
+The intensity and color bleeding of photon mapping for indirect
+illumination are correctly weighted and closely matches the results
+from radiosity.  2 points extra credit.
+  ./render -size 200 200 -input cornell_box_diffuse_sphere.obj -num_photons_to_shoot 500000 -num_shadow_samples 500 -num_photons_to_collect 500 
+
+
+OTHER EXTRA CREDIT [ ?? ]
+1-2 points for new test scene or visualization
+1 point for writing the ray traced image to a file
+1-3 points extra credit for performance improvements
+2-5 points for irradiance caching
+
+<Insert instructions for use and test cases and sample output as appropriate.>
+
+
+
+KNOWN BUGS IN YOUR CODE
+Please be concise!
---
+++ src/radiosity.cpp
@@ -0,0 +1,266 @@
+#include "vectors.h"
+#include "radiosity.h"
+#include "mesh.h"
+#include "face.h"
+#include "sphere.h"
+#include "raytree.h"
+#include "raytracer.h"
+#include "utils.h"
+
+// ================================================================
+// CONSTRUCTOR & DESTRUCTOR
+// ================================================================
+Radiosity::Radiosity(Mesh *m, ArgParser *a) {
+  mesh = m;
+  args = a;
+  num_faces = -1;  
+  formfactors = NULL;
+  area = NULL;
+  undistributed = NULL;
+  absorbed = NULL;
+  radiance = NULL;
+  max_undistributed_patch = -1;
+  total_area = -1;
+  Reset();
+}
+
+Radiosity::~Radiosity() {
+  Cleanup();
+}
+
+void Radiosity::Cleanup() {
+  delete [] formfactors;
+  delete [] area;
+  delete [] undistributed;
+  delete [] absorbed;
+  delete [] radiance;
+  num_faces = -1;
+  formfactors = NULL;
+  area = NULL;
+  undistributed = NULL;
+  absorbed = NULL;
+  radiance = NULL;
+  max_undistributed_patch = -1;
+  total_area = -1;
+}
+
+void Radiosity::Reset() {
+  delete [] area;
+  delete [] undistributed;
+  delete [] absorbed;
+  delete [] radiance;
+
+  // create and fill the data structures
+  num_faces = mesh->numFaces();
+  area = new float[num_faces];
+  undistributed = new Vec3f[num_faces];
+  absorbed = new Vec3f[num_faces];
+  radiance = new Vec3f[num_faces];
+  for (int i = 0; i < num_faces; i++) {
+    Face *f = mesh->getFace(i);
+    f->setRadiosityPatchIndex(i);
+    setArea(i,f->getArea());
+    Vec3f emit = f->getMaterial()->getEmittedColor();
+    setUndistributed(i,emit);
+    setAbsorbed(i,Vec3f(0,0,0));
+    setRadiance(i,emit);
+  }
+
+  // find the patch with the most undistributed energy
+  findMaxUndistributed();
+}
+
+
+// =======================================================================================
+// =======================================================================================
+
+void Radiosity::findMaxUndistributed() {
+  // find the patch with the most undistributed energy 
+  // don't forget that the patches may have different sizes!
+  max_undistributed_patch = -1;
+  total_undistributed = 0;
+  total_area = 0;
+  float max = -1;
+  for (int i = 0; i < num_faces; i++) {
+    float m = getUndistributed(i).Length() * getArea(i);
+    total_undistributed += m;
+    total_area += getArea(i);
+    if (max < m) {
+      max = m;
+      max_undistributed_patch = i;
+    }
+  }
+  assert (max_undistributed_patch >= 0 && max_undistributed_patch < num_faces);
+}
+
+
+void Radiosity::ComputeFormFactors() {
+  assert (formfactors == NULL);
+  assert (num_faces > 0);
+  formfactors = new float[num_faces*num_faces];
+
+
+  // =====================================
+  // ASSIGNMENT:  COMPUTE THE FORM FACTORS
+  // =====================================
+
+
+}
+
+
+// ================================================================
+// ================================================================
+
+float Radiosity::Iterate() {
+  if (formfactors == NULL) 
+    ComputeFormFactors();
+  assert (formfactors != NULL);
+
+
+
+
+  // ==========================================
+  // ASSIGNMENT:  IMPLEMENT RADIOSITY ALGORITHM
+  // ==========================================
+
+
+  // return the total light yet undistributed
+  // (so we can decide when the solution has sufficiently converged)
+  return 0;
+
+
+}
+
+
+
+// =======================================================================================
+// HELPER FUNCTIONS FOR RENDERING
+// =======================================================================================
+
+// for interpolation
+void CollectFacesWithVertex(Vertex *have, Face *f, std::vector<Face*> &faces) {
+  for (unsigned int i = 0; i < faces.size(); i++) {
+    if (faces[i] == f) return;
+  }
+  if (have != (*f)[0] && have != (*f)[1] && have != (*f)[2] && have != (*f)[3]) return;
+  faces.push_back(f);
+  for (int i = 0; i < 4; i++) {
+    Edge *ea = f->getEdge()->getOpposite();
+    Edge *eb = f->getEdge()->getNext()->getOpposite();
+    Edge *ec = f->getEdge()->getNext()->getNext()->getOpposite();
+    Edge *ed = f->getEdge()->getNext()->getNext()->getNext()->getOpposite();
+    if (ea != NULL) CollectFacesWithVertex(have,ea->getFace(),faces);
+    if (eb != NULL) CollectFacesWithVertex(have,eb->getFace(),faces);
+    if (ec != NULL) CollectFacesWithVertex(have,ec->getFace(),faces);
+    if (ed != NULL) CollectFacesWithVertex(have,ed->getFace(),faces);
+  }
+}
+
+// different visualization modes
+Vec3f Radiosity::setupHelperForColor(Face *f, int i, int j) {
+  assert (mesh->getFace(i) == f);
+  assert (j >= 0 && j < 4);
+  if (args->mesh_data->render_mode == RENDER_MATERIALS) {
+    return f->getMaterial()->getDiffuseColor();
+  } else if (args->mesh_data->render_mode == RENDER_RADIANCE && args->mesh_data->interpolate == true) {
+    std::vector<Face*> faces;
+    CollectFacesWithVertex((*f)[j],f,faces);
+    float total = 0;
+    Vec3f color = Vec3f(0,0,0);
+    Vec3f normal = f->computeNormal();
+    for (unsigned int i = 0; i < faces.size(); i++) {
+      Vec3f normal2 = faces[i]->computeNormal();
+      float area = faces[i]->getArea();
+      if (normal.Dot3(normal2) < 0.5) continue;
+      assert (area > 0);
+      total += area;
+      color += float(area) * getRadiance(faces[i]->getRadiosityPatchIndex());
+    }
+    assert (total > 0);
+    color /= total;
+    return color;
+  } else if (args->mesh_data->render_mode == RENDER_LIGHTS) {
+    return f->getMaterial()->getEmittedColor();
+  } else if (args->mesh_data->render_mode == RENDER_UNDISTRIBUTED) { 
+    return getUndistributed(i);
+  } else if (args->mesh_data->render_mode == RENDER_ABSORBED) {
+    return getAbsorbed(i);
+  } else if (args->mesh_data->render_mode == RENDER_RADIANCE) {
+    return getRadiance(i);
+  } else if (args->mesh_data->render_mode == RENDER_FORM_FACTORS) {
+    if (formfactors == NULL) ComputeFormFactors();
+    float scale = 0.2 * total_area/getArea(i);
+    float factor = scale * getFormFactor(max_undistributed_patch,i);
+    return Vec3f(factor,factor,factor);
+  } else {
+    assert(0);
+  }
+  exit(0);
+}
+
+// =======================================================================================
+
+int Radiosity::triCount() {
+  return 12*num_faces;
+}
+
+void Radiosity::packMesh(float* &current) {
+  
+  for (int i = 0; i < num_faces; i++) {
+    Face *f = mesh->getFace(i);
+    Vec3f normal = f->computeNormal();
+
+    //double avg_s = 0;
+    //double avg_t = 0;
+
+    // wireframe is normally black, except when it's the special
+    // patch, then the wireframe is red
+    Vec3f wireframe_color(0,0,0);
+    if (args->mesh_data->render_mode == RENDER_FORM_FACTORS && i == max_undistributed_patch) {
+      wireframe_color = Vec3f(1,0,0);
+    }
+
+    // 4 corner vertices
+    Vec3f a_pos = ((*f)[0])->get();
+    Vec3f a_color = setupHelperForColor(f,i,0);
+    a_color = Vec3f(linear_to_srgb(a_color.r()),linear_to_srgb(a_color.g()),linear_to_srgb(a_color.b()));
+    Vec3f b_pos = ((*f)[1])->get();
+    Vec3f b_color = setupHelperForColor(f,i,1);
+    b_color = Vec3f(linear_to_srgb(b_color.r()),linear_to_srgb(b_color.g()),linear_to_srgb(b_color.b()));
+    Vec3f c_pos = ((*f)[2])->get();
+    Vec3f c_color = setupHelperForColor(f,i,2);
+    c_color = Vec3f(linear_to_srgb(c_color.r()),linear_to_srgb(c_color.g()),linear_to_srgb(c_color.b()));
+    Vec3f d_pos = ((*f)[3])->get();
+    Vec3f d_color = setupHelperForColor(f,i,3);
+    d_color = Vec3f(linear_to_srgb(d_color.r()),linear_to_srgb(d_color.g()),linear_to_srgb(d_color.b()));
+
+    Vec3f avg_color = 0.25f * (a_color+b_color+c_color+d_color);
+    
+    // the centroid (for wireframe rendering)
+    Vec3f centroid = f->computeCentroid();
+    
+    AddWireFrameTriangle(current,
+                         a_pos,b_pos,centroid,
+                         normal,normal,normal,
+                         wireframe_color,
+                         a_color,b_color,avg_color);
+    AddWireFrameTriangle(current,
+                         b_pos,c_pos,centroid,
+                         normal,normal,normal,
+                         wireframe_color,
+                         b_color,c_color,avg_color);
+    AddWireFrameTriangle(current,
+                         c_pos,d_pos,centroid,
+                         normal,normal,normal,
+                         wireframe_color,
+                         c_color,d_color,avg_color);
+    AddWireFrameTriangle(current,
+                         d_pos,a_pos,centroid,
+                         normal,normal,normal,
+                         wireframe_color,
+                         d_color,a_color,avg_color);
+
+  }
+}
+
+// =======================================================================================
